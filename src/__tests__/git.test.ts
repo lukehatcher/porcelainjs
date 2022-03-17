0a4bcb998851606afea7b0de033cdb1813a86bca
Luke Hatcher <lukehatcher98@gmail.com>
2022-03-17 00:13:46 -0700

    Add unit tests + refactor (#1)
    * add basic unit tests -> missing tests for isCopied, isUpdatedButUnmerged and isFileTypeChanged
    * remove unused deps

--- src/__tests__/git.test.ts
+++ src/__tests__/git.test.ts
@@ -1,19 +1,116 @@
 import { Porcelain } from '../index';
-import { execSync } from 'child_process';
+import { exec, execSync } from 'child_process';
+import fs, { writeFileSync } from 'fs';
+import path from 'path';
 
-const testFile = `index.${Math.random()}.js`;
+const testFile = 'index.x.js';
+
+// Paths of files in git must be absolut paths from the root of the git repo.
+const blankFile = path.join('src', '__tests__', 'blank.ts');
+const renamedFile = path.join('src', '__tests__', 'renamed.ts');
 
 describe('porcelainjs', () => {
+  beforeEach(() => {
+    try {
+      execSync(`touch ${testFile}`);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  afterEach(() => {
+    if (fs.existsSync(testFile)) {
+      try {
+        execSync(`rm ${testFile}`);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  });
+
+  afterAll(() => {
+    exec(`git restore --staged ${testFile}`, (e) => {
+      if (e) console.error(e);
+    });
+  });
+
   const fileChecker = new Porcelain();
 
-  it('return true given the name of a modified file', () => {
-    // TODO: cross platform compatiblilty
-    execSync(`touch ${testFile}`);
-    const isThere = fileChecker.isFileInStatus(testFile);
-
-    expect(isThere).toBe(true);
-    // TODO: cross platform compatiblilty
-    execSync(`rm ${testFile}`);
-    execSync(`git ls-files`);
+  it('should return true to show that a newly created file is listed in `git status`', () => {
+    const isInStatus = fileChecker.isFileInStatus(testFile);
+    expect(isInStatus).toBe(true);
   });
+
+  it('should identify an untracked file', () => {
+    const isUntracked = fileChecker.isFileUntracked(testFile);
+    expect(isUntracked).toBe(true);
+  });
+
+  it('should identify an added file', () => {
+    execSync(`git add ${testFile}`);
+    const isAdded = fileChecker.isAdded(testFile);
+    expect(isAdded).toBe(true);
+  });
+
+  it('should identify file as modified (working tree)', () => {
+    execSync(`git add ${testFile}`);
+    const lines = ['asdf', 'asdf', 'asdf'];
+    writeFileSync(testFile, lines.join('\n'));
+
+    const isModified = fileChecker.isModified(testFile, 'working tree');
+    expect(isModified).toBe(true);
+  });
+
+  it('should identify file as modified (working tree)', () => {
+    execSync(`git add ${testFile}`);
+    const lines = ['asdf', 'asdf', 'asdf'];
+    writeFileSync(testFile, lines.join('\n'));
+
+    const isModified = fileChecker.isModified(testFile, 'working tree');
+    expect(isModified).toBe(true);
+  });
+
+  // TODO
+  // it('should identify file as having its type changed (working tree)', () => {
+  //   execSync(`git add ./${testFile}`);
+  //   const lines = ['asdf', 'asdf', 'asdf'];
+  //   writeFileSync(`./${testFile}`, lines.join('\n'));
+
+  //   const isModified = fileChecker.isModified(`./${testFile}`, GitLocation.WORKINGTREE);
+  //   expect(isModified).toBe(true);
+  // });
+
+  it('should identify file as having been deleted', () => {
+    execSync(`git add ${testFile}`);
+    execSync(`rm ${testFile}`);
+
+    const isDeleted = fileChecker.isDeleted(testFile);
+    expect(isDeleted).toBe(true);
+  });
+
+  it('should identify file as having been renamed', () => {
+    execSync(`git mv ${blankFile} ${renamedFile}`);
+
+    const isRenamed = fileChecker.isRenamed(`${blankFile}`);
+    expect(isRenamed).toBe(true);
+    execSync(`git mv ${renamedFile} ${blankFile}`);
+  });
+
+  // TODO
+  // it('should identify file as having been copied', () => {
+  //   // execSync(``);
+
+  //   const isCopied = fileChecker.isCopied(`${blankFile}`);
+  //   expect(isCopied).toBe(true);
+  //   // execSync(``);
+  // });
+
+  // TODO
+  // it('should identify file as having been updated but not merged', () => {
+  //   // execSync(``);
+
+  //   const isUpdatedButUnmerged = fileChecker.isUpdatedButUnmerged(`${blankFile}`);
+  //   expect(isUpdatedButUnmerged).toBe(true);
+  //   // execSync(``);
+  // });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,69 @@
 import { spawnSync, SpawnSyncReturns } from 'child_process';
 
-import { GitLocation, StatusCode } from './types/enums';
+type GitLocation = 'index' | 'working tree';
 
-export class Porcelain {
-  private statusFiles: Map<string, StatusCode> = new Map<string, StatusCode>(); // TODO: type values
+enum StatusCode {
+  UNMODIFIED = ' ',
+  MODIFIED = 'M',
+  FILE_TYPE_CHANGED = 'T',
+  ADDED = 'A',
+  DELETED = 'D',
+  RENAMED = 'R',
+  COPIED = 'C',
+  UPDATED_BUT_UNMERGED = 'U',
+  UNTRACKED = '??',
+}
 
-  // =======================================================
-  // private "util functions"
-  // =======================================================
-  /**
-   * Should this be public???
-   * No, it gets ran after each call
-   */
+interface IPorcelain {
+  isFileInStatus(filename: string): boolean;
+  isFileUntracked(filename: string): boolean;
+  isModified(filename: string, gitLocation?: GitLocation): boolean;
+  isFileTypeChanged(filename: string, gitLocation?: GitLocation): boolean;
+  isAdded(filename: string, gitLocation?: GitLocation): boolean;
+  isDeleted(filename: string, gitLocation?: GitLocation): boolean;
+  isRenamed(filename: string, gitLocation?: GitLocation): boolean;
+  isCopied(filename: string, gitLocation?: GitLocation): boolean;
+  isUpdatedButUnmerged(filename: string, gitLocation?: GitLocation): boolean;
+  get getMap(): Map<string, StatusCode>;
+  get getSet(): Set<string>;
+  get getHashTable(): Record<string, StatusCode>;
+}
+
+export class Porcelain implements IPorcelain {
+  private statusFiles: Map<string, StatusCode> = new Map<string, StatusCode>();
+
   private populateGitStatus(): void {
-    // run command
-    const status: SpawnSyncReturns<Buffer> = spawnSync('git', ['status', '--porcelain', '-z']);
-    // status.stdout.toString().split('\x00');
-    // console.log(status.stdout.toString());
-    const files = status.stdout.toString().split('\x00');
+    const status: SpawnSyncReturns<Buffer> = spawnSync('git', ['status', '--porcelain']);
+    const files = status.stdout.toString().split('\n');
     this.statusFiles.clear();
+
     for (let i = 0; i < files.length - 1; ++i) {
-      const filename = files[i].substring(files[i].lastIndexOf(' ') + 1);
-      const status = files[i].substring(0, files[i].lastIndexOf(' '));
+      const file = files[i];
+      let filename;
+
+      if (file.includes(' -> ')) {
+        // We have a reaname
+        filename = file.substring(
+          3,
+          file.split('').findIndex((_, i) => file.substring(i, i + 4) === ' -> ')
+        );
+      } else {
+        filename = file.substring(file.lastIndexOf(' ') + 1);
+      }
+      const status = file.substring(0, 2);
       this.statusFiles.set(filename, status as StatusCode);
     }
   }
 
   private checkFile(filename: string, statusCode: StatusCode, gitLocation: GitLocation | undefined): boolean {
+    if (filename.startsWith('./')) {
+      filename = filename.substring(2);
+    }
     const gitFile = this.statusFiles.get(filename) ?? false;
 
     if (gitFile) {
       if (gitLocation) {
-        if (gitLocation === GitLocation.INDEX) {
+        if (gitLocation === 'index') {
           return gitFile[0] === statusCode;
         } else {
           return gitFile[1] === statusCode;
@@ -42,9 +74,6 @@ export class Porcelain {
     return false;
   }
 
-  // =======================================================
-  // top level checks
-  // =======================================================
   /**
    * @param filename relative path
    * @returns whether or not the file is editied i.e. the file would show up with `$ git status`
@@ -58,9 +87,6 @@ export class Porcelain {
     return this.statusFiles.has(filename);
   }
 
-  // =======================================================
-  // check if a file is XY
-  // =======================================================
   public isFileUntracked(filename: string): boolean {
     this.populateGitStatus();
     return this.checkFile(filename, StatusCode.UNTRACKED, undefined);
@@ -100,9 +126,6 @@ export class Porcelain {
     return this.checkFile(filename, StatusCode.UPDATED_BUT_UNMERGED, gitLocation);
   }
 
-  // =======================================================
-  // get status files in your preferred DS
-  // =======================================================
   public get getMap(): Map<string, StatusCode> {
     this.populateGitStatus();
     return this.statusFiles;
@@ -120,53 +143,3 @@ export class Porcelain {
     return hashTable;
   }
 }
-
-// const inst = new Porcelain();
-// console.log(inst.isFileInStatus('tsconfig.json'));
-// console.log(inst.isModified('.gitignore'));
-// console.log(inst.populateGitStatus());
-// console.log(ans);
-// console.log('');
-// console.log(ans.split('\x00'));
-
-// =============
-// mapping out what we want to have and work with
-// ========
-
-// base check if a file is modified or not
-
-// features:
-// - pass it a file name to see if its tracked, or get its status
-// - fetch array of all items
-// - fetch set of all items
-// - fetch hset of all items
-//
-
-// want to be able to chain
-// Status.trackedOnly().modifiedOnly().toArray()
-//
-
-// Status.
-
-// use case:
-// - I want to use this with my cli so see if files X Y Z have uncommited changes
-
-// should never just return a
-
-// for i in file name:
-//  if file in list of modifieds:
-//    do thing....
-
-// notes on yarn --porcelain
-// it will list files from the root of the project (but without the ./ prefix)
-// ... this is unlike the raw git status command which will list files with their relative paths
-
-// error handling for when this script is ran in a file that is not a fit repo
-
-// porcelain returns full paths from root: i.e. `git s --porcelain` src folder would list the root tsconfig as `tsconfig.json` and something in types folder as `src/types/blah.ts`
-//
-//
-// need to handle:
-// 1) user gives us relative path (like in git status)
-// 2) user gives path from root
-// 3) user gives path from root but leads with a ./
